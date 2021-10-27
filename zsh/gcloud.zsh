e128de99071806e402e161436f1c08dd5993171b
Alexandre Jouandin <alexandre@jouand.in>
2021-10-27 11:46:00 +0200

    Add GCloud autocompletion

---
+++ zsh/gcloud.zsh
@@ -0,0 +1,1 @@
+if [ -f "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc" ]; then source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"; fi
