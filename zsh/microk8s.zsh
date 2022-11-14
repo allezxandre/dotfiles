4f11913f518d9d5f6328d2db86f32c163f1711d0
Alexandre Jouandin <alexandre@jouand.in>
2022-11-14 12:56:49 +0100

    Remove `microk8s` autocompletion script
    Used to autocomplete `kubectl` and `microk8s` commands with Kubernetes

--- zsh/microk8s.zsh
+++
@@ -1,5 +0,0 @@
-if [ $commands[microk8s.kubectl] ]; then
-    source <(microk8s.kubectl 2>/dev/null >/dev/null && microk8s.kubectl completion zsh | sed 's/__start_kubectl kubectl/__start_kubectl microk8s.kubectl/g')
-    alias mkctl="microk8s.kubectl"
-    complete -F __start_kubectl mkctl
-fi
