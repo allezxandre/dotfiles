7c58232a7f40a69a8d30068ebe6769d8d123a0c0
Alexandre Jouandin <alexandre@jouand.in>
2023-11-09 12:52:48 +0100

    Use Apple Keychain

--- zsh/0000_before.zsh
+++ zsh/0000_before.zsh
@@ -2,8 +2,9 @@
 [[ $TERM == "dumb" ]] && unsetopt zle && return
 # Load any user customizations prior to load
 #
-if [ -d $HOME/.zsh.before/ ]; then
-  if [ "$(ls -A $HOME/.zsh.before/)" ]; then
-    for config_file ($HOME/.zsh.before/*.zsh) source $config_file
-  fi
+if [[ -d $HOME/.zsh.before/ && $(ls -A $HOME/.zsh.before/) ]]; then
+  for config_file in $HOME/.zsh.before/(*.zsh); do source $config_file; done
 fi
+
+# Tell SSH-Add to use Apple Keychain
+ssh-add --apple-use-keychain -q || true
