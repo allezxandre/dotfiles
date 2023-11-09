9da6041a1fe21fe2b086922547b0157c152a1de1
Alexandre Jouandin <alexandre@jouand.in>
2023-11-09 12:53:12 +0100

    Add Tea

--- zsh/0_path.zsh
+++ zsh/0_path.zsh
@@ -1,5 +1,6 @@
 # path, the 0 in the filename causes this to load first
 path=(
+  $HOME/.tea/.local/bin
   /opt/homebrew/bin
   $path
   $HOME/.bin
---
+++ zsh/tea.zsh
@@ -0,0 +1,1 @@
+test -d "$HOME/.tea" && . <("$HOME/.tea/tea.xyz/v*/bin/tea" --magic=zsh --silent)
